96d2a1be97f84902ffd6986adfc88b8435869c24
REDA Laghrissi <93975710+LAGHRISSIREDA@users.noreply.github.com>
2022-10-28 14:52:58 +0100

    docs: Add promotion entity
    propretires && getters-setters && annotations

---
+++ src/main/java/model/Promotion.java
@@ -0,0 +1,16 @@
+package model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.io.Serializable;
+
+@Entity
+public class Promotion implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+}
