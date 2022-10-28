b8b537bcf256378f5e8968eb56015feaed2784a2
REDA Laghrissi <93975710+LAGHRISSIREDA@users.noreply.github.com>
2022-10-28 14:48:45 +0100

    docs: add admin center entity
    make admin center entity with getters && setters && annotation

--- src/main/java/model/AdminCenter.java
+++ src/main/java/model/AdminCenter.java
@@ -9,14 +9,11 @@ import java.io.Serializable;
 import java.util.Objects;
 
 @Entity(name = "admincenter")
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class AdminCenter implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private int id;
+    private long id;
 
     @Basic
     @Column(name = "firstname")
@@ -34,8 +31,19 @@ public class AdminCenter implements Serializable {
     @Column(name = "password")
     private String password;
 
-    //
 
+
+    public AdminCenter() {
+    }
+
+
+
+//    public AdminCenter(String firstname, String lastname, String email, String password) {
+//        this.firstname = firstname;
+//        this.lastname = lastname;
+//        this.email = email;
+//        this.password = password;
+//    }
 
     @Override
     public boolean equals(Object o) {
@@ -48,5 +56,47 @@ public class AdminCenter implements Serializable {
     @Override
     public int hashCode() {
         return Objects.hash(id, firstname, lastname, email, password);
+    }
+
+    //getters and setters
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
