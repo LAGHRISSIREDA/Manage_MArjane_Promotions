c5c24e5d202b15d3be7e2a5b3a8598f370c82d37
REDA Laghrissi <93975710+LAGHRISSIREDA@users.noreply.github.com>
2022-10-28 14:54:03 +0100

    docs: Add SubCategory entity
    propreties && getters-setters && annotation

---
+++ src/main/java/model/SubCategory.java
@@ -0,0 +1,18 @@
+package model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.io.Serializable;
+
+@Entity
+public class SubCategory implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String nameSubCategorie;
+}
